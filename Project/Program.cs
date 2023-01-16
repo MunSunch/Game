c8f7541882b05abf92271a421e87e139dd666361
MunirTheBest <msunchalyaev@gmail.com>
2023-01-16 12:13:36 +0300

    Add Karta and Fabric pack of Karta

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,9 +1,10 @@
 ﻿namespace Project
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            
+        }
     }
-  }
 }
